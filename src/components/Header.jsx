36ecf46a5566563b8140111dcb58e20edf459ed9
dominic <dominijisumbonafe@gmail.com>
2024-11-29 12:03:46 +0800

    minor touchup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,18 +30,17 @@ function Header() {
           <li>
             <HeaderLink openMenu={openMenu} text="Blogs" path="blogs" />
           </li>
-          {openMenu && (
-            <li>
-              <a
-                href="/Resume.pdf"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`rounded-sm ${openMenu ? "rounded-none border-none transition-all duration-75 hover:shadow-[0_1px_0_0_#23a8b1]" : "border-[#2cd2dd]"} bg-transparent px-3 py-2 text-xs uppercase text-[#2cd2dd] hover:border-[#23a8b1] hover:text-[#23a8b1] oldPhone:px-2 oldPhone:py-1 android:text-sm md:text-lg`}
-              >
-                View Resume
-              </a>
-            </li>
-          )}
+
+          <li>
+            <a
+              href="/Resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`rounded-sm ${openMenu ? "rounded-none border-none transition-all duration-75 hover:shadow-[0_1px_0_0_#23a8b1]" : "border-[#2cd2dd]"} bg-transparent px-3 py-2 text-xs uppercase text-[#2cd2dd] hover:border-[#23a8b1] hover:text-[#23a8b1] oldPhone:px-2 oldPhone:py-1 android:text-sm md:text-lg`}
+            >
+              View Resume
+            </a>
+          </li>
         </div>
       </menu>
 
